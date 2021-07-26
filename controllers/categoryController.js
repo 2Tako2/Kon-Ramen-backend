308c2995b0bfc6290e6da321fe7204116f9a3b3c
felix ngo <felixhmngo@gmail.com>
2021-07-26 15:51:09 +1000

    fixed populate error in categoryController

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,7 +3,7 @@ const Category = require('../models/Category.js');
 
 const getCategories = async (req, res) => {
     try {
-        const categories = await Category.find();
+        const categories = await Category.find().populate('items');
         res.status(200).json(categories);
     } catch (err) {
         res.status(500).json({ message: err.message });
